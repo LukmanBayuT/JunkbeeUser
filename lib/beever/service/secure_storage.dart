e00d8252dec4c15c7e0161a111384c9fc93daed2
Lukman Bayu Tresnadi <lukmanbayutresnadi@gmail.com>
2022-03-17 10:06:25 +0700

    Commit dikit

--- lib/beever/service/secure_storage.dart
+++ lib/beever/service/secure_storage.dart
@@ -1,7 +1,7 @@
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 class SecureStorage {
-  final _storage = FlutterSecureStorage();
+  final _storage = const FlutterSecureStorage();
 
   Future writeSecureData(String key, String value) async {
     var writeData = await _storage.write(key: key, value: value);
--- lib/main.dart
+++ lib/main.dart
@@ -2,17 +2,13 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
-<<<<<<< Updated upstream
-import 'package:junkbee_user/view/splashscreen/1.%20onboarding_splash_screen.dart';
-import 'package:junkbee_user/view/pages/0.navigator.dart';
-=======
 import 'package:junkbee_user/user/view/splashscreen/1.%20onboarding_splash_screen.dart';
->>>>>>> Stashed changes
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+  SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
   runApp(const MyApp());
 }
 
@@ -29,7 +25,7 @@ class MyApp extends StatelessWidget {
         textSelectionTheme:
             const TextSelectionThemeData(cursorColor: Color(0xff707070)),
       ),
-      home: const NavigatorUser(),
+      home: const OnboardingSplashScreen(),
     );
   }
 }
