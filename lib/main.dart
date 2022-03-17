f9625f2a30d5ed076bcd669df2c04c60f6616850
Pratama <55432533+Pratama-lab@users.noreply.github.com>
2022-03-17 10:11:16 +0700

    add assets beever

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
-<<<<<<< Updated upstream
-import 'package:junkbee_user/view/splashscreen/1.%20onboarding_splash_screen.dart';
-import 'package:junkbee_user/view/pages/0.navigator.dart';
-=======
 import 'package:junkbee_user/user/view/splashscreen/1.%20onboarding_splash_screen.dart';
->>>>>>> Stashed changes
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -29,7 +24,7 @@ class MyApp extends StatelessWidget {
         textSelectionTheme:
             const TextSelectionThemeData(cursorColor: Color(0xff707070)),
       ),
-      home: const NavigatorUser(),
+      home: const OnboardingSplashScreen(),
     );
   }
 }
