b4f15a797a4257a76c5d4c1f35326d02c4651508
Lukman Bayu Tresnadi <lukmanbayutresnadi@gmail.com>
2022-03-29 21:57:07 +0700

    lalalala nglembur

--- lib/beever/views/pages/ongoing_order/ongoing_order_proceed_final.dart
+++ lib/beever/views/pages/ongoing_order/ongoing_order_proceed_final.dart
@@ -1,22 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:junkbee_user/beever/views/pages/ongoing_order/ongoing_order_proceed_final.dart';
 import 'package:junkbee_user/user/constant/constant.dart';
 import 'package:location/location.dart';
 import 'package:page_transition/page_transition.dart';
 import 'package:sliding_up_panel/sliding_up_panel.dart';
 import 'package:swipeable_button_view/swipeable_button_view.dart';
 
-class OngoingOrderProceed extends StatefulWidget {
+class OngoingOrderProceedFinal extends StatefulWidget {
   var panelController = PanelController();
 
-  OngoingOrderProceed({Key? key}) : super(key: key);
+  OngoingOrderProceedFinal({Key? key}) : super(key: key);
 
   @override
-  State<OngoingOrderProceed> createState() => _OngoingOrderProceedState();
+  State<OngoingOrderProceedFinal> createState() =>
+      _OngoingOrderProceedFinalState();
 }
 
-class _OngoingOrderProceedState extends State<OngoingOrderProceed> {
+class _OngoingOrderProceedFinalState extends State<OngoingOrderProceedFinal> {
+  final TextEditingController _jenisSampah = TextEditingController();
   final panelController = PanelController();
   LocationData? currentLocation;
   late LocationData destinationLocation;
@@ -39,7 +40,7 @@ class _OngoingOrderProceedState extends State<OngoingOrderProceed> {
               currentLocation!.longitude ?? 0.0)
           : const LatLng(-6.9714229, 110.4265293),
     );
-    final panelHeightOpen = MediaQuery.of(context).size.height * 0.35;
+    final panelHeightOpen = MediaQuery.of(context).size.height * 0.8;
     final panelHeightClosed = MediaQuery.of(context).size.height * 0.1;
 
     return Scaffold(
@@ -210,75 +211,133 @@ class _OngoingOrderProceedState extends State<OngoingOrderProceed> {
                   height: 20,
                   thickness: 2,
                 ),
-                (isOnTheWay == true)
-                    ? Padding(
-                        padding: const EdgeInsets.all(20.0),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            SizedBox(
-                                width: MediaQuery.of(context).size.width / 2.6,
-                                height: MediaQuery.of(context).size.height / 13,
-                                child: ElevatedButton(
-                                  style: ElevatedButton.styleFrom(
-                                      primary: Colors.red,
-                                      shape: roundedRectBor),
-                                  child: const Text('Cancel',
-                                      style: onboardingGetStarted),
-                                  onPressed: () {},
-                                )),
-                            const SizedBox(
-                              width: 20,
-                            ),
-                            SizedBox(
-                                width: MediaQuery.of(context).size.width / 2.6,
-                                height: MediaQuery.of(context).size.height / 13,
-                                child: ElevatedButton(
-                                  style: ElevatedButton.styleFrom(
-                                      primary: Colors.amber,
-                                      shape: roundedRectBor),
-                                  child: const Text('Pick Up',
-                                      style: onboardingGetStarted),
-                                  onPressed: () {},
-                                )),
-                          ],
-                        ),
-                      )
-                    : Container(),
-                (isOnPickUp == false)
-                    ? Padding(
-                        padding: const EdgeInsets.all(20),
-                        child: SwipeableButtonView(
-                          buttonText: 'Arrived at The Pickup',
-                          buttonWidget: Container(
-                            child: const Icon(
-                              Icons.arrow_forward_ios_rounded,
-                              color: Colors.grey,
+                Container(
+                  margin: const EdgeInsets.symmetric(horizontal: 20),
+                  height: MediaQuery.of(context).size.height * 0.45,
+                  width: MediaQuery.of(context).size.width / 1,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text(
+                        'Category',
+                        style: bodyBodyBold,
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      TextFormField(
+                        decoration: InputDecoration(
+                          hintText: 'Jenis Sampah',
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(16.7),
+                            borderSide: const BorderSide(
+                              color: Colors.blue,
                             ),
                           ),
-                          activeColor: const Color.fromARGB(255, 247, 172, 12),
-                          isFinished: isFinished,
-                          onWaitingProcess: () {
-                            Future.delayed(const Duration(seconds: 2), () {
-                              setState(() {
-                                isFinished = true;
-                              });
-                            });
-                          },
-                          onFinish: () async {
-                            await Navigator.push(
-                                context,
-                                PageTransition(
-                                    type: PageTransitionType.fade,
-                                    child: OngoingOrderProceedFinal()));
-                            //TODO: For reverse ripple effect animation
-                            setState(() {
-                              isFinished = false;
-                            });
-                          },
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(16.7),
+                            borderSide: const BorderSide(
+                              color: Color(0xFFDEDEDE),
+                              width: 2.0,
+                            ),
+                          ),
+                          errorBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(16.7),
+                            borderSide: const BorderSide(
+                              color: Colors.red,
+                              width: 2.0,
+                            ),
+                          ),
+                          focusedErrorBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(16.7),
+                            borderSide: const BorderSide(
+                              color: Colors.red,
+                              width: 2.0,
+                            ),
+                          ),
+                          counterText: '',
                         ),
-                      )
-                    : Container()
+                        controller: _jenisSampah,
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      const Text(
+                        'Total Weight',
+                        style: bodyBodyBold,
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      TextFormField(
+                        decoration: InputDecoration(
+                          hintText: 'Jenis Sampah',
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(16.7),
+                            borderSide: const BorderSide(
+                              color: Colors.blue,
+                            ),
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(16.7),
+                            borderSide: const BorderSide(
+                              color: Color(0xFFDEDEDE),
+                              width: 2.0,
+                            ),
+                          ),
+                          counterText: '',
+                        ),
+                        controller: _jenisSampah,
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      SizedBox(
+                          width: MediaQuery.of(context).size.width / 4,
+                          height: MediaQuery.of(context).size.height / 13,
+                          child: ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                                primary: Colors.amber, shape: roundedRectBor),
+                            child: Text('Confirm',
+                                style: onboardingGetStarted.copyWith(
+                                    fontSize: 18)),
+                            onPressed: () {},
+                          )),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(20),
+                  child: SwipeableButtonView(
+                    buttonText: 'Weight Confirmation',
+                    buttonWidget: Container(
+                      child: const Icon(
+                        Icons.arrow_forward_ios_rounded,
+                        color: Colors.grey,
+                      ),
+                    ),
+                    activeColor: const Color.fromARGB(255, 247, 172, 12),
+                    isFinished: isFinished,
+                    onWaitingProcess: () {
+                      Future.delayed(const Duration(seconds: 2), () {
+                        setState(() {
+                          isFinished = true;
+                        });
+                      });
+                    },
+                    onFinish: () async {
+                      await Navigator.push(
+                          context,
+                          PageTransition(
+                              type: PageTransitionType.fade,
+                              child: OngoingOrderProceedFinal()));
+                      //TODO: For reverse ripple effect animation
+                      setState(() {
+                        isFinished = false;
+                      });
+                    },
+                  ),
+                )
               ],
             ),
           );
